4be8f1700e0cb3b2d7e25671483c803966c7a10f
Alex Krudu <KrudAlex@yandex.ru>
2018-07-02 19:44:50 +0300

    Added map displaying

---
+++ app/src/main/java/com/example/teen/swheel/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.teen.swheel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("59ac5e3f-0809-43e6-90be-94f6c4ec8742");
+        MapKitFactory.initialize(this);
+
+        // Укажите имя activity вместо map.
+        setContentView(R.layout.activity_main);
+        mapview = (MapView)findViewById(R.id.mapview);
+        mapview.getMap().move(
+                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
+    }
+    @Override
+    protected void onStop() {
+        // Activity onStop call must be passed to both MapView and MapKit instance.
+        mapview.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        // Activity onStart call must be passed to both MapView and MapKit instance.
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapview.onStart();
+    }
+}
+
+
