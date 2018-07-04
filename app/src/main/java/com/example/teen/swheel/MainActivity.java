556dbcd571264a446f28f0ac5d2001c35a47f820
= <=>
2018-07-04 17:01:13 +0400

    Profile page

--- app/src/main/java/com/example/teen/swheel/MainActivity.java
+++ app/src/main/java/com/example/teen/swheel/MainActivity.java
@@ -1,49 +1,28 @@
 package com.example.teen.swheel;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
-import com.yandex.mapkit.Animation;
-import com.yandex.mapkit.MapKitFactory;
-import com.yandex.mapkit.geometry.Point;
-import com.yandex.mapkit.map.CameraPosition;
-import com.yandex.mapkit.mapview.MapView;
-
-
-public class MainActivity extends AppCompatActivity {
-
-    private MapView mapview;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        MapKitFactory.setApiKey("59ac5e3f-0809-43e6-90be-94f6c4ec8742");
-        MapKitFactory.initialize(this);
-
-        // Укажите имя activity вместо map.
         setContentView(R.layout.activity_main);
-        mapview = (MapView)findViewById(R.id.mapview);
-        mapview.getMap().move(
-                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
-                new Animation(Animation.Type.SMOOTH, 0),
-                null);
-    }
-    @Override
-    protected void onStop() {
-        // Activity onStop call must be passed to both MapView and MapKit instance.
-        mapview.onStop();
-        MapKitFactory.getInstance().onStop();
-        super.onStop();
+
     }
 
     @Override
-    protected void onStart() {
-        // Activity onStart call must be passed to both MapView and MapKit instance.
-        super.onStart();
-        MapKitFactory.getInstance().onStart();
-        mapview.onStart();
+    public void onClick(View v){
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void button_click(View v){
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        startActivity(intent);
     }
 }
-
-
---
+++ app/src/main/java/com/example/teen/swheel/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.teen.swheel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+    public void onClick(View view) {
+    }
+}
