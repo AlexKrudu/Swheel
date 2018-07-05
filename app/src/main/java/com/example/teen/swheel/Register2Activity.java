54b430fca6e3d25abe262cd5c58a41920c7e03bf
= <=>
2018-07-05 13:53:16 +0300

    Profile page

--- app/src/main/java/com/example/teen/swheel/Register2Activity.java
+++ app/src/main/java/com/example/teen/swheel/Register2Activity.java
@@ -1,17 +1,14 @@
 package com.example.teen.swheel;
 
+
 import android.arch.persistence.room.Room;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class Register2Activity extends AppCompatActivity {
 
